6f1917c26b12fe956911afe775335dc0fed1da24
Will Hopkins <38540736+willothy@users.noreply.github.com>
2020-08-08 18:14:37 -0700

    First Commit
    Project setup

---
+++ core/Core/Program.cs
@@ -0,0 +1,21 @@
+﻿using ElectronCgi.DotNet;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder()
+                .WithLogging()
+                .Build();
+            
+            connection.On("greeting", (string name) => 
+            {
+                return $"Hello {name}";
+            });
+            
+            connection.Listen();    
+        }
+    }
+}
